7bd5c73b0d7c73230ab95d8ce54e7fbfc87b965b
aisha1ii <aisha.chwimy@gmail.com>
2026-06-30 08:00:14 +0000

    tutorial_6

---
+++ tutorial_6/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Lecturer l = new Lecturer("L100", "Dr Aisha", "OOP", "Faculty of Information Technology");
+        l.displayInfo();
+        l.displaySubject();
+        l.displayDepartment();
+    }
+}
